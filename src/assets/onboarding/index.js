5628477ce3cb91910ef920921e78acb5d329c2e3
Nkwuda Theophilus <theophilusonyebuchinkwuda@gmail.com>
2024-10-16 16:17:10 +0100

    quick fix

--- src/assets/onboarding/index.js
+++ src/assets/onboarding/index.js
@@ -1,7 +1,7 @@
 export { default as robot } from './robot.svg';
 export {default as coperator } from './coperator.svg';
 export {default as trophy} from './trophy.svg';
-export {default as bgvector } from './vectorbg.svg';
+export {default as bgvector } from './Vectorbg.svg';
 export {default as dancing } from './dancing.svg';
 export { default as moon} from './moon.svg';
 export { default as lady } from './lady.svg';
